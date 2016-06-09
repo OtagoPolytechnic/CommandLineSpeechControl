abc04a6beac55e22fa379868dcbbe0dc1bff4ac2
gregfield <gregfield1996@gmail.com>
2016-06-09 12:39:24 +1200

    Looking for sound in background thread

--- Python Speech To Text/t.py
+++ Python Speech To Text/t.py
@@ -1,17 +1,57 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
+#-----------------------------------------
+#Multi-threading function 
+#start of speech recognition
+#-----------------------------------------
 
-with sr.Microphone() as source:              
-     audio = r.listen(source)
-      	
-x = 1
+def speechRecog(recogniser, audio):
+ 
+		try:
+			audioText = recogniser.recognize_google(audio)
+			findCommandWords(audioText)
+		except LookupError:                            # speech is unintelligible
+			print("Could not understand audio")
+		except sr.UnknownValueError:
+			print("Google Speech Recognition could not understand the audio")
+		except sr.RequestError:
+			print("Could not request results from Google Speech Recognition")
 
-while True:	                  
-    try:  
-        print(r.recognize_google(audio) + "\n") 
-        x += 1
-        break
-        
-    except LookupError:                           	
-        print("Could not understand audio")
+			
+#-----------------------------------------
+#run all words through the reg expressions 
+#to try find any command words
+#-----------------------------------------
+
+def findCommandWords(audioString):
+	
+	#audioString.find('dash')
+	#if 'dash' in audioString:
+	#	print("-")
+		
+	print(audioString)
+
+
+#-----------------------------------------
+#start of speech recognition
+#-----------------------------------------
+
+recogniser = sr.Recognizer()
+#os.system("start /wait cmd")
+
+#set default microphone as the source for audio
+with sr.Microphone() as mic:
+
+	#adjust the threshold for ambient noise in the room
+	#so that it doesnt try and recognize random noise into words
+	print("Adjusting for ambient room noise")
+	recogniser.adjust_for_ambient_noise(mic)
+
+print("Speak when ready")
+
+stopListening = recogniser.listen_in_background(mic, speechRecog)
+
+while (True):
+	var = 1
+	
+stopListening()
