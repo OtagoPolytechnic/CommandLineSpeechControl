b124d36bc4d372afaf681b2fa4fe9af31a0466c9
gregfield <gregfield1996@gmail.com>
2016-06-13 16:02:08 +1200

    Started working on word replacement

--- Python Speech To Text/t.py
+++ Python Speech To Text/t.py
@@ -1,5 +1,17 @@
 import speech_recognition as sr
 
+#---------------------------------------------
+#Word class to hold all the command words and 
+#what they need to be replaced with
+#---------------------------------------------
+
+class Word:
+
+	def __init__(self , word, replacement):
+		self.word = word
+		self.replacement = replacement
+	
+	
 #-----------------------------------------
 #Multi-threading function 
 #start of speech recognition
@@ -10,14 +22,14 @@ def speechRecog(recogniser, audio):
 	try:
 		audioText = recogniser.recognize_google(audio)
 		findCommandWords(audioText)
-	except LookupError:                            # speech is unintelligible
+	except LookupError:            
 		print("Could not understand audio")
 	except sr.UnknownValueError:
 		print("Google Speech Recognition could not understand the audio")
 	except sr.RequestError:
 		print("Could not request results from Google Speech Recognition")
 
-			
+		
 #-----------------------------------------
 #run all words through the reg expressions 
 #to try find any command words
@@ -25,19 +37,20 @@ def speechRecog(recogniser, audio):
 
 def findCommandWords(audioString):
 	
-	#audioString.find('dash')
-	#if 'dash' in audioString:
-	#	print("-")
+	#searching the list of words
+	alteredString = audioString
+	
+	for words in wordsList:
+		alteredString = alteredString.replace(words.word, words.replacement)
 		
-	print(audioString)
+	print(alteredString)
 
 
 #-----------------------------------------
-#start of speech recognition
+#Setting up Speech Recognition
 #-----------------------------------------
 
 recogniser = sr.Recognizer()
-#os.system("start /wait cmd")
 
 #set default microphone as the source for audio
 with sr.Microphone() as mic:
@@ -49,9 +62,17 @@ with sr.Microphone() as mic:
 
 print("Speak when ready")
 
+#starts the method to make a background thread and continually search for sound
 stopListening = recogniser.listen_in_background(mic, speechRecog)
 
+#adding command words to a list
+wordsList = []
+wordsList.append(Word("dash","-"))
+wordsList.append(Word("change directory", "cd"))
+
+#keeps the program running
 while (True):
 	var = 1
-	
+
+#stops the background listening	
 stopListening()
