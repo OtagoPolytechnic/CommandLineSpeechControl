a5dc2aaf35f54c6199468dbc400239f37d6876d5
gregfield <gregfield1996@gmail.com>
2016-11-04 12:52:51 +1300

    Start/Stop Keys now user settable

--- Developers/gui.py
+++ Developers/gui.py
@@ -18,7 +18,11 @@ class SpeechGui(tkinter.Tk):
 			#makes an instance of the speech recognition class 
 			self.recognizeSpeech = speech.SpeechToCommandLine()
 			#boolean so only 1 speech recognition thread can be activated at once
-			self.recognitionRunning = False;
+			self.recognitionRunning = False
+			self.setStartKeyFlag = False
+			self.setStopKeyFlag = False
+			self.startKey = 'F1'
+			self.stopKey = 'F2'
 		except Exception as e:
 			print("Could not Load Application")
 			print(str(e))
@@ -27,15 +31,22 @@ class SpeechGui(tkinter.Tk):
 		
 	#sets the dimensions for the UI and adds all the objetcs to it
 	def initialize(self):
-		self.geometry("400x235")
+		self.geometry("400x300")
 		self.grid()
 		
-		#buttons for start, stop, and pause
-		startButton = tkinter.Button(self, text=u"Start Speech Recognition (F1)", command=self.onStartBtnClick, height=5, width=24)
+		#buttons for start and stop
+		startButton = tkinter.Button(self, text=u"Start Speech Recognition", command=self.onStartBtnClick, height=5, width=24)
 		startButton.grid(column=0,row=1, sticky='W', padx=10, pady=10)
 				
-		stopButton = tkinter.Button(self, text=u"Stop Speech Recognition (F2)", command=self.onStopBtnClick, height=5, width=24)
+		stopButton = tkinter.Button(self, text=u"Stop Speech Recognition", command=self.onStopBtnClick, height=5, width=24)
 		stopButton.grid(column=1,row=1, sticky='W', padx=10, pady=10)
+		
+		#buttons for setting the starting and stopping keys for the speech recognition
+		startKeySetButton = tkinter.Button(self, text=u"Set Start Key", command=self.onSetStartBtnClick, height=2, width=24)
+		startKeySetButton.grid(column=0,row=6, sticky='W', padx=10, pady=10)
+		
+		stopKeySetButton = tkinter.Button(self, text=u"Set Stop Key", command=self.onSetStopBtnClick, height=2, width=24)
+		stopKeySetButton.grid(column=1,row=6, sticky='W', padx=10, pady=10)
 		
 		#label for whatradio buttons to select speech engine/api
 		apiLabel = tkinter.Label(self, text=u"Please select a speech recognition engine:", anchor='w')
@@ -78,13 +89,44 @@ class SpeechGui(tkinter.Tk):
 			messagebox.showinfo("Not Running!","Speech Recognition is not currently running")
 			print("Not Running")
 	
+	#When the on set start key is clicked it sets the set start key flag to true
+	def onSetStartBtnClick(self):
+		self.setStartKeyFlag = True
+		print("Press key to set as start key")
+	
+	#When the on set stop key is clicked it sets the set stp key flag to true
+	def onSetStopBtnClick(self):
+		self.setStopKeyFlag = True
+		print("Press key to set as stop key")
+	
+	#Sets the clicked key to the key for starting the recognition
+	def setStartKey(self, event):
+		self.startKey = event.Key
+		self.setStartKeyFlag = False
+		print("Key Set")
+	
+	#Sets the clicked key to the key for stopping the recognition
+	def setStopKey(self, event):
+		self.stopKey = event.Key
+		self.setStopKeyFlag = False
+		print("Key Set")
+		
 	#Function for when a key is pressed on the keyboard to see if it is a command key
 	def keyBoardPress(self, event):
-		if event.Key == 'F1':
-			self.onStartBtnClick()
-		if event.Key == 'F2':
-			self.onStopBtnClick()
-		
+		#For if the setting either of the keys flag is true it calls the events to set the keys
+		if (self.setStartKeyFlag == True) or (self.setStopKeyFlag == True):
+			if self.setStartKeyFlag == True:
+				self.setStartKey(event)
+			if self.setStopKeyFlag == True:
+				self.setStopKey(event)
+				
+		#else it will just check the key against the starting and stopping keys
+		else:
+			if event.Key == self.startKey:
+				self.onStartBtnClick()
+			if event.Key == self.stopKey:
+				self.onStopBtnClick()
+						
 		return True
 
 		
